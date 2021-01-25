291b15476f945ebdf6f85166b2a17b219a7f5c34
Kishare <kicshary@gmail.com>
2021-01-25 03:47:37 +0100

    add completedtests

--- test/test.js
+++ test/test.js
@@ -20,7 +20,61 @@ const payloadColor = () => {
   nextCursor++;
   return { 'color': nextColor }
 }
-const getCurrentCulor = () => {
+const getCurrentColor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
 // <-- FIN
+
+describe('GET /colors', () => {
+  it('should return all colors', (done) =>{
+    chai.request(app)
+    .get('/colors').end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.an('object');
+        response.body.results.should.be.an('array');
+        response.body.results.should.be.eql(['RED', 'GREEN', 'BLUE']);
+        done();
+    })
+  })
+})
+
+describe('GET /invalid', () => {
+  it('should return Bad Request', (done) =>{
+    chai.request(app)
+    .get('/lsjdflsd,').end((error, response) =>{
+      response.should.have.status(404);
+      done();
+    })
+  })
+})
+
+describe('POST /colors', () => {
+  it('should add new color', (done) =>{
+    chai.request(app)
+    .post('/colors')
+    .send(payloadColor())
+    .end((error, response) =>{
+      response.should.have.status(201);
+      response.should.be.json;
+      response.body.should.be.an('object');
+      response.body.results.should.be.an('array');
+      response.body.results.should.contain(getCurrentColor());
+      done();
+    })
+  })
+})
+
+describe('GET /colors', () => {
+  it('should return new color list Request', (done) =>{
+    chai.request(app)
+    .get('/colors').end((error, response) =>{
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.an('object');
+      response.body.results.should.be.an('array');
+      response.body.results.should.be.eql(['RED', 'GREEN', 'BLUE', getCurrentColor()]);
+      done();
+    })
+  })
+})
